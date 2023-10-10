b8c7e1a4892e31713d9cd84d4645d8ce10deebf2
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-10 11:47:16 -0500

    Import primeNG modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// PrimeNG Modules
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { MenubarModule } from 'primeng/menubar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    RippleModule,
+    AutoCompleteModule,
+    FormsModule,
+    MenubarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
