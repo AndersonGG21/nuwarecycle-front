25f973fd8e1f27394ff42ce43823b145c1ba4265
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-10 11:46:11 -0500

    Add Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sunsetOrange : {
+          50: '#f2652260',
+          100: '#f2652280',
+          200: '#f26522',
+          300: '##DC4903',
+          400: '#B43A00',
+          500: '#A73803',
+        }
+      },
+      fontFamily: {
+        'anybody': ['Anybody'],
+        'anybody-italic': ['AnybodyItalic'],
+        'anybody-bold': ['AnybodyBold'],
+        'anybody-thin': ['AnybodyThin'],
+      }
+    },
   },
   plugins: [],
 }
