eaa61cc9a3b17a381d4100d341508a01b464006f
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-16 21:13:54 -0500

    Add avatar & sidebar menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { AutoCompleteCompleteEvent } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Login',
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-login-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2"></path><path d="M3 12h13l-3 -3"></path><path d="M13 15l3 -3"></path></svg>',
+        escape: false,
+        // command: () => {
+        //     this.update();
+        // }
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-times',
+        // command: () => {
+        //     this.delete();
+        // }
+      },
+    ];
+  }
   items: any[] = [];
 
   menuItems: MenuItem[] | undefined;
@@ -15,6 +34,8 @@ export class NavbarComponent {
   selectedItem: any = {};
 
   suggestions: any[] = [];
+
+  sidebarVisible: boolean = false;
 
   search(event: AutoCompleteCompleteEvent) {
     this.suggestions = [...Array(10).keys()].map(
