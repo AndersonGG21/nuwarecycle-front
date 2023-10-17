14eea974202426c167678dc0146da7752a9450a3
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-16 21:12:59 -0500

    Create Bento Grid component

---
+++ src/app/components/bento-grid/bento-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bento-grid',
+  templateUrl: './bento-grid.component.html',
+  styleUrls: ['./bento-grid.component.css']
+})
+export class BentoGridComponent {
+
+}
