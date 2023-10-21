0ff33df1be419b8dddf45c29da816cc36cd4c24c
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-21 09:47:05 -0500

    Add product list and grid

--- src/app/components/pages/products/products.component.ts
+++ src/app/components/pages/products/products.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+export type Product = {
+  name : string,
+  img : string,
+  price : number,
+  description : string
+}
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
+  products : Product[] = []
+  layout: string = 'list';
+
+  ngOnInit(): void {
+    this.products = [{name : "Producto 1", img : "https://via.placeholder.com/150", price : 100, description : "Descripcion del producto 1"}, {name : "Producto 2", img : "https://via.placeholder.com/150", price : 200, description : "Descripcion del producto 2"},  {name : "Producto 3", img : "https://via.placeholder.com/150", price : 300, description : 'No'}, {name : "Producto 4", img : "https://via.placeholder.com/150", price : 400, description : 'No'}, {name : "Producto 5", img : "https://via.placeholder.com/150", price : 500, description : 'No'}]
+  }
+
+  changeLayout() {
+    this.layout = this.layout === 'list' ? 'grid' : 'list';    
+  }
 }
