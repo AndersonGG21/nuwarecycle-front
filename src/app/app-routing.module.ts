08abbcacc9f2ea39a7287380b2be8328ef313667
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-21 09:45:32 -0500

    Add route to products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/pages/landing/landing.component';
+import { ProductsComponent } from './components/pages/products/products.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: LandingComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },    
+    { path: 'home', component: LandingComponent},    
+    { path: 'products', component: ProductsComponent}
+
 ];
 
 @NgModule({
