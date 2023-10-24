73e7a4386f60e54f033754ea2bd6a8d8b2de7a9a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-23 22:50:35 -0500

    Remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,42 +16,9 @@ export class AppComponent implements OnInit {
 
   constructor(private primengConfig: PrimeNGConfig) {}
 
-  items: any[] = [];
-
-  selectedItem: any = {};
-
-  suggestions: any[] = [];
-
-  search(event: AutoCompleteCompleteEvent) {
-    this.suggestions = [...Array(10).keys()].map(
-      (item) => event.query + '-' + item
-    );
-    console.log(this.selectedItem);
-  }
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
 
-    const listItem = document.querySelectorAll('li');
-    const menuBackdrop = document.querySelector(
-      '#menu-backdrop'
-    ) as HTMLDivElement;
-
-    listItem.forEach((item) => {
-      item.addEventListener('mouseenter', ({ target }: any) => {
-        const { left, top, width, height } = item.getBoundingClientRect();
-        menuBackdrop.style.setProperty('--left', `${left}px`);
-        menuBackdrop.style.setProperty('--top', `${top}px`);
-        menuBackdrop.style.setProperty('--width', `${width}px`);
-        menuBackdrop.style.setProperty('--alto', `${height}px`);
-        menuBackdrop.style.visibility = 'visible';
-        menuBackdrop.style.opacity = '1';
-      });
-
-      item.addEventListener('mouseleave', () => {
-        menuBackdrop.style.visibility = 'hidden';
-        menuBackdrop.style.opacity = '0';
-      });
-    });
   }
 }
