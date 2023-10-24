105a367d6df62fdbea193ceb432a6daa6f1cc487
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-23 21:45:47 -0500

    Add temp route to test product page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/pages/landing/landing.component';
 import { ProductsComponent } from './components/pages/products/products.component';
+import { ProductComponent } from './components/pages/product/product.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },    
     { path: 'home', component: LandingComponent},    
-    { path: 'products', component: ProductsComponent}
+    { path: 'products', component: ProductsComponent},
+    { path: 'product', component: ProductComponent},
 
 ];
 
