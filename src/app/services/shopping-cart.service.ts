87d5b1dc1ee2d5e2bb3e88b68438ded67fada9d9
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-29 22:30:26 -0500

    Add shopping cart methods

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
 
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  stock: number;
+  image1: string;
+  image2: string;
+  image3: string;
+  image4: string;
+  brand: string;
+  rating: number;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingCartService {
+  constructor() {}
 
-  constructor() { }
+  //Create an empty list of products
+  cartProducts: Product[] = [];
+
+  //Add product to the list
+  addToCart(product: Product) {
+    this.cartProducts.push(product);
+    console.log(product);
+    console.log(this.cartProducts);
+  }
+
+  //Get the list of products
+  getCartProducts() {
+    return this.cartProducts;
+  }
+
+  //delete product from the list
+  deleteProduct(product: Product) {
+    const index = this.cartProducts.indexOf(product);
+    if (index !== -1) {
+      this.cartProducts.splice(index, 1);
+    }
+  }
 }
