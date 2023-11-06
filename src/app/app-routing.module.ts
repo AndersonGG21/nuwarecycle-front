5647b2899d3a9f4b1b7734b450f2ad25f4147930
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-11-06 18:00:30 -0500

    Add cart component to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/pages/landing/landing.component';
 import { ProductsComponent } from './components/pages/products/products.component';
 import { ProductComponent } from './components/pages/product/product.component';
+import { CartComponent } from './components/pages/cart/cart.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },    
     { path: 'home', component: LandingComponent},    
     { path: 'products', component: ProductsComponent},
     { path: 'product', component: ProductComponent},
+    { path: 'cart', component: CartComponent}
 
 ];
 
