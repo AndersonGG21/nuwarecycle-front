6c95babaa494be457de1fdefb475767c46461d3a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-11-19 22:04:17 -0500

    Refactor search functionality and add product redirection

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { AutoCompleteCompleteEvent } from 'primeng/autocomplete';
 import { LoginServiceService } from 'src/app/services/login-service.service';
+import { ProductsServiceService } from 'src/app/services/products-service.service';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 import { Product } from 'src/app/type';
 
@@ -12,23 +14,37 @@ import { Product } from 'src/app/type';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  
   items: any[] = [];
-  productAmount : number = 0;
   menuItems: MenuItem[] | undefined;
-  selectedItem: any = {};
-  suggestions: any[] = [];
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    image: '',
+    category: '',
+    stock: 0,
+    image1: '',
+    image2: '',
+    image3: '',
+    image4: '',
+    brand: '',
+    rating: 0,
+  };
   sidebarVisible: boolean = false;
   visible: boolean = false;
   loginVisible: boolean = false;
   private cartService = inject(ShoppingCartService);
   badge$ = '0';
-  shoppingCartProducts: Product[] = [];  
+  filteredProds: Product[] = [];
+  shoppingCartProducts: Product[] = [];
   private fb: FormBuilder = inject(FormBuilder);
-  credentials: FormGroup = this.fb.group({});  
+  credentials: FormGroup = this.fb.group({});
   private loginService = inject(LoginServiceService);
+  private productService = inject(ProductsServiceService);
+  private router = inject(Router);
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.menuItems = [
       {
         label: 'Login',
@@ -36,16 +52,18 @@ export class NavbarComponent implements OnInit {
         escape: false,
         command: () => {
           this.loginVisible = true;
-        }
+        },
       },
       {
         label: 'Delete',
         icon: 'pi pi-times',
-        command: () => {
-          
-        }
+        command: () => {},
       },
     ];
+
+    this.productService.getAllProducts().subscribe((res) => {
+      this.filteredProds = res;
+    });
 
     this.credentials = this.fb.group({
       email: ['', [Validators.email, Validators.required]],
@@ -53,8 +71,8 @@ export class NavbarComponent implements OnInit {
     });
 
     this.cartService.getCartLenght().subscribe((res) => {
-      this.badge$ = res.toString();      
-    })
+      this.badge$ = res.toString();
+    });
 
     this.shoppingCartProducts = this.cartService.getCartProducts();
   }
@@ -63,18 +81,33 @@ export class NavbarComponent implements OnInit {
     this.visible = true;
   }
 
-  search(event: AutoCompleteCompleteEvent) {
-    this.suggestions = [...Array(10).keys()].map(
-      (item) => event.query + '-' + item
-    );
-    console.log(this.selectedItem);
-  }
-
   deleteProduct(id: number) {
     this.cartService.deleteProductById(id);
   }
 
   submit() {
-    this.loginService.login(this.credentials.value.email, this.credentials.value.password);
+    this.loginService.login(
+      this.credentials.value.email,
+      this.credentials.value.password
+    );
+  }
+
+  filterProducts(event: AutoCompleteCompleteEvent) {
+    let filtered: any[] = [];
+    let query = event.query;
+
+    for (let i = 0; i < (this.filteredProds as any[]).length; i++) {
+      let product = (this.filteredProds as any[])[i];
+      if (product.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(product);
+      }
+    }
+
+    this.filteredProds = filtered;
+  }
+
+  redirectToProduct() : void {
+    const productName = this.selectedProduct?.name
+    this.router.navigate([`/product/${productName}`])
   }
 }
