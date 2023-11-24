e6cdf429849eba895638491b69cea0fb9f3fa20a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-11-23 21:03:06 -0500

    Refactor login service to store user information in cookies

--- src/app/services/login-service.service.ts
+++ src/app/services/login-service.service.ts
@@ -2,12 +2,21 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
+interface User {
+  id : number,
+  username: string,
+  email : string,
+  rol: string,
+  profileImg : string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
 
   private LOGIN_API_URL = "http://localhost:8080/api/v1/login";
+  private USERS_API_URL = "http://localhost:8080/api/v1/users";
   private http = inject(HttpClient);
   private cookie = inject(CookieService);
   private options = {
@@ -16,10 +25,6 @@ export class LoginServiceService {
   }
 
   login(email: string, password: string) {
-    let credentials = {
-      email: email,
-      password: password
-    }
     this.http.post< HttpResponse<any> >(this.LOGIN_API_URL, {email, password}, {observe : 'response', headers : this.options.headers}).subscribe(
       (response) => {
         const token = response.headers.get("Authorization");
@@ -29,6 +34,13 @@ export class LoginServiceService {
         }
       }, error => {
         alert("Wrong username or password");
+      }, () => {
+        this.http.get<User>(`${this.USERS_API_URL}/byEmail/${email}`).subscribe((user) => {
+          this.cookie.set("uid", user.id.toString());
+          this.cookie.set("username", user.username);
+          this.cookie.set("rol", user.rol);
+          this.cookie.set("profileImg", user.profileImg);                    
+        })
       }
     )
   }
